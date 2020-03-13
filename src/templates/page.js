bbd8780625dee66d195b5b58798ae5c571ad44a5
Judith Hartmann <judith.hartmann@futurice.com>
2020-03-13 16:47:09 +0100

    Dynamically generate pages from contentfulPage content type

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,38 +1,35 @@
 import { graphql } from 'gatsby';
 import { useIntl } from 'gatsby-plugin-intl';
-import React, { FC } from 'react';
+import React from 'react';
 
-import { PetitionsPageQuery } from '../../types/graphql-types';
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 import PageContent from '../components/pageContent';
 
-interface Props {
-  data: PetitionsPageQuery;
-}
-
-const PetitionsPage: FC<Props> = ({ data }) => {
+const Page = ({ data }) => {
+  console.log(data.contentfulPage.node_locale);
   const intl = useIntl();
   return (
     <Layout>
       <SEO
-        title={data.contentfulPage?.title!}
+        title={data.contentfulPage.title}
         description={data.contentfulPage?.seoDescription ?? undefined}
         lang={intl.locale}
       />
-      <PageContent data={data.contentfulPage!} />
+      <PageContent data={data.contentfulPage} />
     </Layout>
   );
 };
 
 export const query = graphql`
-  query PetitionsPage($locale: String) {
-    contentfulPage(slug: { eq: "petitions" }, node_locale: { eq: $locale }) {
+  query PageQuery($slug: String, $locale: String) {
+    contentfulPage(slug: { eq: $slug }, node_locale: { eq: $locale }) {
       ...pageInformation
       title
       seoDescription
+      node_locale
     }
   }
 `;
 
-export default PetitionsPage;
+export default Page;
