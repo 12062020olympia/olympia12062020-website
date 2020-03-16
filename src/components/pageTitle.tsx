8b452d86e304f5a9309f8f25e614371212218877
Judith Hartmann <judith.hartmann@futurice.com>
2020-03-16 13:33:55 +0100

    Use inner container for page title content positioning

--- src/components/pageTitle.tsx
+++ src/components/pageTitle.tsx
@@ -59,31 +59,36 @@ const Container = styled(BackgroundImage)`
 
 const TitleContainer = styled.div`
   background-color: ${colors.PageTitleBackground};
+  margin: ${top.sm} ${contentMargin.sm} ${bottom.sm} ${contentMargin.sm};
+  width: calc(100% - 2 * ${contentMargin.sm});
+
+  ${applyMediaQueryMd(css`
+    margin: ${top.md} ${contentMargin.md} ${bottom.md} ${contentMargin.md};
+    width: min-content;
+  `)}
+
+  ${applyMediaQueryLg(css`
+    margin: ${top.lg} ${contentMargin.lg} ${bottom.lg} ${contentMargin.lg};
+  `)}
+`;
+
+const InnerContainer = styled.div`
   flex-direction: column;
   justify-content: flex-end;
-  margin: ${top.sm} ${contentMargin.sm} ${bottom.sm} ${contentMargin.sm};
   padding-top: 20px;
   padding-bottom: 15px;
-  width: calc(100% - 2 * ${contentMargin.sm});
 
   > * {
     padding: 0 10px;
   }
 
   ${applyMediaQueryMd(css`
-    margin: ${top.md} ${contentMargin.md} ${bottom.md} ${contentMargin.md};
     padding-top: 25px;
     padding-bottom: 25px;
-    width: min-content;
 
     > * {
       padding: 0 30px;
     }
-  `)}
-
-  ${applyMediaQueryLg(css`
-    margin: ${top.lg} ${contentMargin.lg} ${bottom.lg} ${contentMargin.lg};
-    width: min-content;
   `)}
 `;
 
@@ -91,8 +96,10 @@ const PageTitle: FC<Props> = ({ backgroundPicture, title, header }) => (
   // @ts-ignore
   <Container fluid={backgroundPicture?.fluid ?? undefined}>
     <TitleContainer>
-      <Title type="pageTitle" title={title!} />
-      <Title type="h1" title={header!} />
+      <InnerContainer>
+        <Title type="pageTitle" title={title!} />
+        <Title type="h1" title={header!} />
+      </InnerContainer>
     </TitleContainer>
   </Container>
 );
